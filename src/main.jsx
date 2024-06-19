6bfc35af9ef22154b8fefd2dbe59d6c2761ebe17
Lesia <150070945+Cassel444@users.noreply.github.com>
2024-06-19 14:24:09 +0300

    init repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store";
+import { store } from "./redux/store";
 import App from "./components/App/App.jsx";
-import Loader from "./components/Loader/Loader.jsx";
+// import Loader from "./components/Loader/Loader.jsx";
 import "./index.css";
 import "modern-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<Loader />} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
